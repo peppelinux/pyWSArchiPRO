8ac5da39009b5ad51ff6066db65e469e073252ac
Giuseppe De Marco <giuseppe.demarco@unical.it>
2020-05-28 12:33:54 +0200

    cleanup and TODO

--- protocollo_ws/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-# from django.utils.translation import gettext as _
-
-class ProtocolloWsConfig(AppConfig):
-    name = 'protocollo_ws'
-    verbose_name = 'Protocollo Ws'
-
